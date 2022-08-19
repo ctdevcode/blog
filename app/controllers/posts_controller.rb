d348d41e935af1cde78e23c30d8695060a031458
ctdevcode <ctdevcode@gmail.com>
2022-08-18 18:25:26 -0600

    Init

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,23 @@
+class PostsController < ApplicationController
+  def show
+		@post = Post.find(params[:id])
+  end
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
