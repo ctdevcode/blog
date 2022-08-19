f3effbe8e7a7781749560de1d28a98c5b6653ed3
ctdevcode <ctdevcode@gmail.com>
2022-08-18 23:12:17 -0600

    Posts created and styled

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
+	before_action :set_post, only: [:show, :edit, :update, :destroy]
+	
   def show
-		@post = Post.find(params[:id])
   end
 
   def index
@@ -12,7 +13,7 @@ class PostsController < ApplicationController
   end
 
   def create
-		@post = Post.create(params.require(:post).permit(:title, :body))
+		@post = Post.create(post_params)
 		if @post.save
 			flash[:notice] = "Post was created successfully!"
 			redirect_to @post
@@ -26,12 +27,33 @@ class PostsController < ApplicationController
   end
 
   def update
+		if @post.update(post_params)
+			flash[:notice] = "Post was edited successfully!"
+			redirect_to @post
+		else 
+			flash.now[:alert] = "Post was not edited"
+			render 'edit'
+		end
   end
 
   def destroy
+		@post.destroy
+		flash[:notice] = "The post was deleted successfully"
+		redirect_to timeline_path
   end
 
 	def timeline
 		@posts = Post.all
 	end
+
+
+		private	
+
+		def set_post
+			@post = Post.find(params[:id])
+		end
+
+		def post_params
+			params.require(:post).permit(:title, :body)
+		end
 end
